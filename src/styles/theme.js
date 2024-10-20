d4a5c3fb85de6f7f1095466de875aeda3b38dcfe
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-20 17:21:51 +0300

    Add secondary colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,11 +3,15 @@ import { colors } from "./colors";
 export const lightTheme = {
   backgroundColor: colors.backgroundLight,
   textColor: colors.textDark,
+  secondaryTextColor: colors.secondaryTextLight,
   primary: colors.primary,
+  secondary: colors.secondary,
 };
 
 export const darkTheme = {
   backgroundColor: colors.backgroundDark,
   textColor: colors.textLight,
+  secondaryTextColor: colors.secondaryTextDark,
   primary: colors.primary,
+  secondary: colors.secondary,
 };
