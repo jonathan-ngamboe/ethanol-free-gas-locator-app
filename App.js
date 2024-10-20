3c63f9c0cace58ab32783ebe805b162d2a61008d
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-21 00:28:29 +0300

    Add SafeAreaProvider to correctly manage safe areas on iOS

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { ThemeProvider, useTheme } from './src/context/ThemeContext';
 import { useGlobalStyles } from './src/styles/globalStyles';
@@ -18,50 +19,52 @@ function AppContent() {
 
   return (
     <PaperProvider theme={theme}>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            headerStyle: styles.header,
-            headerTitleStyle: styles.headerTitle,
-            tabBarStyle: styles.tabBar,
-            tabBarActiveTintColor: theme.colors.primary,
-          }}
-        >
-            <Tab.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                tabBarLabel: 'Home',
-                headerTitleAlign: 'left',
-                tabBarIcon: ({ color, size }) => {
-                  return <Icon name="home-variant-outline" size={size} color={color} />;
-                },
-              }}
-            /> 
-            <Tab.Screen
-              name="Discover"
-              component={DiscoverScreen}
-              options={{
-                tabBarLabel: 'Discover',
-                headerShown: false,
-                tabBarIcon: ({ color, size }) => {
-                  return <Icon name="map-search" size={size} color={color} />;
-                },
-              }}
-            />
-            <Tab.Screen 
-              name="Profile" 
-              component={ProfileScreen} 
-              options={{
-                tabBarLabel: 'Profile',
-                headerTitleAlign: 'left',
-                tabBarIcon: ({ color, size }) => {
-                  return <Icon name="account" size={size} color={color} />;
-                },
-              }}
-            />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              headerStyle: styles.header,
+              headerTitleStyle: styles.headerTitle,
+              tabBarStyle: styles.tabBar,
+              tabBarActiveTintColor: theme.colors.primary,
+            }}
+          >
+              <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  tabBarLabel: 'Home',
+                  headerTitleAlign: 'left',
+                  tabBarIcon: ({ color, size }) => {
+                    return <Icon name="home-variant-outline" size={size} color={color} />;
+                  },
+                }}
+              /> 
+              <Tab.Screen
+                name="Discover"
+                component={DiscoverScreen}
+                options={{
+                  tabBarLabel: 'Discover',
+                  headerShown: false,
+                  tabBarIcon: ({ color, size }) => {
+                    return <Icon name="map-search" size={size} color={color} />;
+                  },
+                }}
+              />
+              <Tab.Screen 
+                name="Profile" 
+                component={ProfileScreen} 
+                options={{
+                  tabBarLabel: 'Profile',
+                  headerTitleAlign: 'left',
+                  tabBarIcon: ({ color, size }) => {
+                    return <Icon name="account" size={size} color={color} />;
+                  },
+                }}
+              />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </PaperProvider>
   );
 }
