cd383cd937f36bcdb49bc4ed5b44b489da0d21be
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-20 16:51:45 +0300

    Add DiscoverScreen with MapView

---
+++ src/screens/DiscoverScreen.js
@@ -0,0 +1,10 @@
+import MapView, { Marker } from "react-native-maps";
+
+
+export default function DiscoverScreen() {
+    return (
+        <MapView
+            style={{width: '100%', height: '100%'}}
+        />
+    );
+}
