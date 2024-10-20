b58e78b9cb5c36101d5e36e3d0529fd15f902776
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-20 16:53:41 +0300

    Add theme.js with light and dark theme colors

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+import { colors } from "./colors";
+
+export const lightTheme = {
+  backgroundColor: colors.backgroundLight,
+  textColor: colors.textDark,
+  primary: colors.primary,
+};
+
+export const darkTheme = {
+  backgroundColor: colors.backgroundDark,
+  textColor: colors.textLight,
+  primary: colors.primary,
+};
