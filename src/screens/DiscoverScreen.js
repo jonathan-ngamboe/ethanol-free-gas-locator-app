7cbd71897cf3a72e8502cf18fc25cefe00601e39
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-21 02:18:47 +0300

    Add Card with searchbar

--- src/screens/DiscoverScreen.js
+++ src/screens/DiscoverScreen.js
@@ -1,10 +1,90 @@
+import React from "react";
 import MapView, { Marker } from "react-native-maps";
-
+import { Card, Searchbar } from 'react-native-paper';
+import { useGlobalStyles } from '../styles/globalStyles';
+import { View, StyleSheet, Text } from "react-native";
+import { DEFAULT_REGION, ZOOM_LEVELS } from '../constants/mapConstants';
 
 export default function DiscoverScreen() {
+    const styles = useGlobalStyles();
+
     return (
-        <MapView
-            style={{width: '100%', height: '100%'}}
-        />
+        <View style={localStyles.container}>
+            <MapView
+                style={localStyles.map}
+                initialRegion={DEFAULT_REGION}
+                zoomEnabled={true}
+                zoomTapEnabled={true}
+                zoomControlEnabled={true}
+                zoomLevel={ZOOM_LEVELS.CITY}
+            />
+            <View style={localStyles.overlay}>
+                <Card style={{...localStyles.card, ...styles.cardBackgroundColor}}>
+                    <Searchbar
+                        placeholder="Search near ethanol stations"
+                        inputStyle={localStyles.searchInput}
+                        style={{...localStyles.searchBar, ...styles.searchBar}}
+                        //onChangeText={setSearchQuery}
+                        //value={searchQuery}
+                    />
+                    <Card.Content style={{alignItems: 'center', justifyContent: 'center', marginTop: 20}}>
+                        <Text style={styles.textColor}>Results will be displayed here</Text>
+                    </Card.Content>     
+                </Card>
+            </View>
+        </View>
     );
 }
+
+const localStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position: 'relative',
+    },
+
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+
+    overlay: {
+        position: 'absolute',
+        bottom: 15, 
+        width: '100%',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+
+    card: {
+        borderRadius: 20,
+        width: '100%',
+        elevation: 4, // For Android  
+        shadowColor: '#000', // For iOS
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        padding: 20,
+    },
+
+    cardTitle: {
+        fontSize: 18,
+    },
+
+    searchBar: {
+        backgroundColor: 'transparent',
+        borderWidth: 1,
+        height: 36, 
+        justifyContent: 'center',
+        paddingHorizontal: 8,
+    },
+
+    searchInput: {
+        fontSize: 14,
+        minHeight: 0, 
+        paddingBottom: 0, 
+        textAlignVertical: 'center', 
+    },
+});
