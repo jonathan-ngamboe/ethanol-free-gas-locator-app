f44d9ddea626183ed5e5cc9bddd33f5ef27fca56
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-21 02:17:13 +0300

    Add Map Constants

---
+++ src/constants/mapConstants.js
@@ -0,0 +1,12 @@
+export const DEFAULT_REGION = {
+    latitude: 40.7128,
+    longitude: -74.0060,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+};
+
+export const ZOOM_LEVELS = {
+    CITY: 0.0922,
+    NEIGHBORHOOD: 0.0421,
+    STREET: 0.01,
+};
