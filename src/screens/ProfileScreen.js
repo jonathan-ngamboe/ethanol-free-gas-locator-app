e1e2202935ff5074040ed10964bc8ced110ed63a
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-20 21:08:53 +0300

    Use theme context directly in globalStyles instead of props for dynamic styling and add button to test themes.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,13 +1,18 @@
 import { View, Text } from 'react-native';
-import { lightTheme, darkTheme } from '../styles/theme';
-import { globalStyles } from '../styles/globalStyles';
+import { useGlobalStyles } from '../styles/globalStyles';
+import { useTheme } from '../context/ThemeContext';
+import { Button } from 'react-native';
 
 export default function ProfileScreen() {
-    const currentTheme = lightTheme;
+    const styles = useGlobalStyles();
+    const { toggleTheme } = useTheme();
 
     return (
-        <View style={globalStyles(currentTheme).container}>
-            <Text>Profile Screen</Text>
+        <View style={styles.container}>
+            <Button 
+                title="Toggle Theme"
+                onPress={toggleTheme}
+            />
         </View>
     );
 }
