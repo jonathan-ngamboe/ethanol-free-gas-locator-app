1401b32984ea74a50be926f1c6fff3e9ddd9e313
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-21 00:45:47 +0300

    Add Card Title with avatar to redirect to Profile Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Pressable } from 'react-native';
+import { Avatar, Card } from 'react-native-paper';
 import { useGlobalStyles } from '../styles/globalStyles';
 
-export default function HomeScreen() {
+
+export default function HomeScreen({navigation}) {
     const styles = useGlobalStyles();
-    
+
     return (
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
+        <View style={ styles.container }>
+            <Card.Title
+                title="👋 Welcome"
+                titleVariant="titleMedium"
+                style={{...localStyles.cardTitle, ...styles.cardOnSurfaceColor}}
+                titleStyle={{...localStyles.titleCard, ...styles.cardTitle}}
+                right={(props) =>   <Pressable onPress={() => navigation.navigate('Profile')} > 
+                                        <Avatar.Icon 
+                                            {...props} 
+                                            icon="account" 
+                                            size={45} 
+                                            onPress={() => navigation.navigate('Profile')} 
+                                            style={styles.avatar} 
+                                        />
+                                    </Pressable>
+                }
+            />
         </View>
     );
 }
+
+const localStyles = StyleSheet.create({
+    cardTitle: {
+        position: 'absolute',
+        top: 0,
+        marginTop: 16,
+        borderRadius: 45,
+        minHeight: 45, 
+    },
+
+    titleCard: {
+        position: 'absolute',
+    },
+});
