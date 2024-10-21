2be8a3b882b98a3c0042754fe88a78c7bcee9097
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-21 20:43:19 +0300

    Add navigation

--- src/screens/DiscoverScreen.js
+++ src/screens/DiscoverScreen.js
@@ -3,14 +3,14 @@ import { View, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
 import CardSearchList from '../components/CardSearchList';
 import Map from "../components/Map";
 
-export default function DiscoverScreen() {
+export default function DiscoverScreen({navigation}) {
 
     return (
         <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{flex: 1}}>
             <View style={localStyles.container}>
                 <Map/>
                 <View style={localStyles.overlay}>
-                    <CardSearchList/>
+                    <CardSearchList navigation={navigation}/>
                 </View>
             </View>
         </KeyboardAvoidingView>
