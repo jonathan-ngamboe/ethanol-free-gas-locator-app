5362ba755126e829db3ed93ad8930b5bf1a74778
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-22 23:58:14 +0300

    Replace Card by SearchBar component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,49 +1,27 @@
-import { View, StyleSheet, Pressable } from 'react-native';
-import { Avatar, Card } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
 import { useGlobalStyles } from '../styles/globalStyles';
+import SearchBar from '../components/searchBar';
 
 
 export default function HomeScreen({navigation}) {
     const styles = useGlobalStyles();
-    const user = { 
-        firstName: 'John', 
-        lastName: 'Doe',
-        email: 'john@example.com',
-        isPro: false,
-    };
 
     return (
         <View style={ styles.container }>
-            <Card.Title
-                title="👋 Welcome"
-                titleVariant="titleMedium"
-                style={{...localStyles.cardTitle, ...styles.cardBackgroundColor}}
-                titleStyle={{...localStyles.titleCard, ...styles.cardTitle}}
-                right={(props) =>   <Pressable onPress={() => navigation.navigate('Profile')} > 
-                                        <Avatar.Image 
-                                            source={{ uri: user?.avatar ? user.avatar : `https://avatar.iran.liara.run/public` }}
-                                            {...props} 
-                                            size={45} 
-                                            onPress={() => navigation.navigate('Profile')} 
-                                            style={styles.avatar} 
-                                        />
-                                    </Pressable>
-                }
-            />
+            <View style={localStyles.searchBarContainer}>
+                    <SearchBar navigation={navigation}/>
+                </View>
         </View>
     );
 }
 
 const localStyles = StyleSheet.create({
-    cardTitle: {
+    searchBarContainer: {
         position: 'absolute',
-        top: 0,
-        marginTop: 16,
-        borderRadius: 45,
-        minHeight: 45, 
-    },
-
-    titleCard: {
-        position: 'absolute',
+        top: 15,  
+        left: 0,
+        right: 0,
+        alignItems: 'center',
+        zIndex: 1, 
     },
 });
