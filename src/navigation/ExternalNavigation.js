ae0f98bdc7f0e8d09a5d858b7d3ecf67674522e0
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-22 04:28:56 +0300

    Add ExternalNavigation component with function to open the phone app

---
+++ src/navigation/ExternalNavigation.js
@@ -0,0 +1,27 @@
+import { Alert, Linking, Platform } from 'react-native';
+
+export function OpenMap({ destinationName, destinationLat, destinationLon }) {
+    console.log('OpenMap', destinationName, destinationLat, destinationLon);
+    const scheme = Platform.select({ ios: 'maps://0,0?q=', android: 'geo:0,0?q=' });
+    const latLng = `${destinationLat},${destinationLon}`;
+    const label = destinationName;
+
+    const url = Platform.select({
+        ios: `${scheme}${label}@${latLng}`,
+        android: `${scheme}${latLng}(${label})`
+        })
+    
+    const openLink = async () => {
+      // Checking if the link is supported for links with custom URL scheme.
+      const supported = await Linking.canOpenURL(url);
+  
+      if (supported) {
+        // Opening the link with map app, 
+        await Linking.openURL(url);
+      } else {
+        Alert.alert(`No navigation app found to show directions to ${label}`);
+      }
+    };
+
+    openLink();        
+}
