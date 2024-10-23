0c7edd0fbbfdffd3fdbfa157054181c62c8114df
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-23 12:24:04 +0300

    Replace ProfileScreen by ProfileScreen to navigate to nested screens

--- src/navigation/stacks/HomeStack.js
+++ src/navigation/stacks/HomeStack.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useTheme } from '../../context/ThemeContext';
 import { useGlobalStyles } from '../../styles/globalStyles';
 import HomeScreen from '../../screens/HomeScreen';
-import ProfileScreen from '../../screens/ProfileScreen';
+import { ProfileStack } from './ProfileStack';
 
 const Stack = createStackNavigator();
 
@@ -29,17 +28,9 @@ export const HomeStack = () => {
       />
       <Stack.Screen 
         name="Profile" 
-        component={ProfileScreen}
+        component={ProfileStack}
         options={{
-          headerTitle: "",
-          headerRight: () => (
-            <Icon 
-              name="pencil"
-              size={24} 
-              style={{ marginRight: 20 }} 
-              color={theme.colors.primary} 
-            />
-          ),
+          headerShown: false,
         }}
       />
     </Stack.Navigator>
