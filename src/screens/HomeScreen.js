7b711e98baf81422953f73e18f2308840d0c82fb
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-24 23:56:41 +0300

    Refactor HomeScreen component to add user avatar to the SearchBar and navigate to map on focus

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,47 @@
 import { View, StyleSheet } from 'react-native';
 import { useGlobalStyles } from '../styles/globalStyles';
 import SearchBar from '../components/SearchBar';
+import { Avatar } from 'react-native-paper';
+import { useIsFocused } from '@react-navigation/native'; 
 
 
 export default function HomeScreen({navigation}) {
     const styles = useGlobalStyles();
 
+    const isFocused = useIsFocused();
+
+    const user = { 
+        firstName: 'Monkey D.',
+        lastName: 'Luffy',
+        email: 'monkey-d@one-piece.rock',
+        avatar: 'https://i.pinimg.com/736x/1e/8b/f3/1e8bf3b2adefdfe76bb5dfe9bafe1ed5.jpg',
+    };
+
+    const searchBarRightComponent = (
+        <Avatar.Image 
+            source={{ uri: user?.avatar || 'https://avatar.iran.liara.run/public' }}
+            size={45} 
+            style={styles.avatar} 
+        />
+    );
+
+    const navigateToMap = () => {
+        navigation.navigate('Discover', { searchQuery: '' });
+    };
+
     return (
         <View style={ styles.container }>
             <View style={localStyles.searchBarContainer}>
-                    <SearchBar navigation={navigation}/>
-                </View>
+                {isFocused && 
+                    <SearchBar
+                        leftIcon='map-marker'
+                        onLeftIconPress={navigateToMap}
+                        rightComponent={searchBarRightComponent}
+                        onRightComponentPress={() => navigation.navigate('ProfileStack')}
+                        onFocus={navigateToMap} 
+                        />
+                }
+            </View>
         </View>
     );
 }
