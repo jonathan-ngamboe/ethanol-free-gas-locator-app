38aa0892e42b2fbb132b35c0d5845d8e9518ba5b
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-24 18:04:52 +0300

    Update list styles and add dividers

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import { useGlobalStyles } from '../styles/globalStyles';
 import { View, StyleSheet, ScrollView, Platform } from 'react-native';
-import { useTheme, Button, Switch, List } from 'react-native-paper';
+import { useTheme, Button, List, Divider } from 'react-native-paper';
 import ProfileCard from '../components/ProfileCard';
 import { share } from '../navigation/ExternalNavigation';
 import { openLink } from '../navigation/ExternalNavigation';
@@ -27,51 +27,55 @@ export default function ProfileScreen({ navigation }) {
                     <List.Item
                         title={user.firstName + ' ' + user.lastName}
                         description={user.email}
-                        left={props => <List.Icon {...props} icon="account-multiple-plus" color={theme.colors.primary} style={localStyles.contentPaddingLeft} />}
-                        right={(props) => <Button {...props} icon="plus" mode="contained" onPress={() => share('Hey, Join me on this app!')}>Invite</Button>}
-                        titleStyle={localStyles.listTitle}
+                        left={props => <List.Icon {...props} icon="account-multiple-plus" color={theme.colors.primary} style={styles.contentPaddingLeft} />}
+                        right={(props) => <Button {...props} icon="plus" mode="contained" textColor={theme.colors.background} onPress={() => share('Hey, Join me on this app!')}>Invite</Button>}
+                        titleStyle={styles.listTitle}
                         titleVariant='bodySmall'
-                        subtitleStyle={{...localStyles.listDescription, color: theme.colors.outline }}
+                        subtitleStyle={{...styles.listDescription, color: theme.colors.outline }}
                         subtitleVariant='bodySmall'
                     />
 
-                    <List.Section title='Stations' style={{...localStyles.listSection}} titleStyle={{...localStyles.listTitle, color: theme.colors.outline, ...localStyles.contentPaddingLeft}}>
+                    <Divider style={styles.divider} />
+
+                    <List.Section title='Stations' style={{...styles.listSection}} titleStyle={{...styles.listTitle, color: theme.colors.outline, ...styles.contentPaddingLeft}}>
                         <List.Item
                             title="My favorite stations"
-                            titleStyle={{ ...localStyles.listTitle }}
-                            left={props => <List.Icon {...props} icon="heart" color={theme.colors.primary} style={localStyles.contentPaddingLeft} />}
-                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={localStyles.contentPaddingRight}/>}
+                            titleStyle={{ ...styles.listTitle }}
+                            left={props => <List.Icon {...props} icon="heart" color={theme.colors.primary} style={styles.contentPaddingLeft} />}
+                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={styles.contentPaddingRight}/>}
                             onPress={() => navigation.navigate('Favorites')}
                         />
                         <List.Item
                             title="My search history"
-                            titleStyle={{ ...localStyles.listTitle }}
-                            left={props => <List.Icon {...props} icon="history" color={theme.colors.primary} style={localStyles.contentPaddingLeft} />}
-                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={localStyles.contentPaddingRight}/>}
+                            titleStyle={{ ...styles.listTitle }}
+                            left={props => <List.Icon {...props} icon="history" color={theme.colors.primary} style={styles.contentPaddingLeft} />}
+                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={styles.contentPaddingRight}/>}
                             onPress={() => navigation.navigate('History')}
                         />
                     </List.Section>
 
-                    <List.Section title='More' style={{...localStyles.listSection}} titleStyle={{...localStyles.listTitle, color: theme.colors.outline, ...localStyles.contentPaddingLeft}}>
+                    <Divider style={styles.divider} />
+
+                    <List.Section title='More' style={{...styles.listSection}} titleStyle={{...styles.listTitle, color: theme.colors.outline, ...styles.contentPaddingLeft}}>
                         <List.Item
                             title="Rate the app"
-                            titleStyle={{ ...localStyles.listTitle }}
-                            left={props => <List.Icon {...props} icon="star-outline" color={theme.colors.primary} style={localStyles.contentPaddingLeft} />}
-                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={localStyles.contentPaddingRight}/>}
+                            titleStyle={{ ...styles.listTitle }}
+                            left={props => <List.Icon {...props} icon="star-outline" color={theme.colors.primary} style={styles.contentPaddingLeft} />}
+                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={styles.contentPaddingRight}/>}
                             onPress={() => openLink(Platform.select({ ios: 'https://apps.apple.com/us/app/ethanol-free-gas-locator-app', android: 'https://play.google.com/store/apps/details?id=com.ethanol-free-gas-locator-app' }))}
                         />
                         <List.Item
                             title="Logout"
-                            titleStyle={{ ...localStyles.listTitle }}
-                            left={props => <List.Icon {...props} icon="logout" color={theme.colors.primary} style={localStyles.contentPaddingLeft} />}
-                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={localStyles.contentPaddingRight}/>}
+                            titleStyle={{ ...styles.listTitle }}
+                            left={props => <List.Icon {...props} icon="logout" color={theme.colors.primary} style={styles.contentPaddingLeft} />}
+                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={styles.contentPaddingRight}/>}
                             onPress={() => console.log('Logout')}
                         />
                         <List.Item
                             title="Delete account"
-                            titleStyle={{ ...localStyles.listTitle }}
-                            left={props => <List.Icon {...props} icon="delete" color={theme.colors.primary} style={localStyles.contentPaddingLeft} />}
-                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={localStyles.contentPaddingRight} />}
+                            titleStyle={{ ...styles.listTitle }}
+                            left={props => <List.Icon {...props} icon="delete" color={theme.colors.primary} style={styles.contentPaddingLeft} />}
+                            right={(props) => <List.Icon {...props} icon="chevron-right" color={theme.colors.primary} style={styles.contentPaddingRight} />}
                             onPress={() => console.log('Delete account')}
                         />  
                     </List.Section>
@@ -86,28 +90,5 @@ const localStyles = StyleSheet.create({
     mainView: {
         width: '100%',
         height: '100%',
-    },
-
-    listTitle: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        lineHeight: 0,
-    },
-
-    listDescription: {
-        fontSize: 14,
-        lineHeight: 0,
-    },
-
-    contentPaddingLeft: {
-        paddingLeft: 30,
-    },
-
-    contentPaddingRight: {
-        paddingRight: 20,
-    },
-
-    listSection: {
-        marginTop: 10,
     },
 });
