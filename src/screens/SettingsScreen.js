6f9be41ca59747d0e62d886fc1afe9a4b5dd3cfc
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-25 14:41:59 +0300

    Add start screen selection and email notification toggle

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,7 +1,7 @@
 import { View, ScrollView, Platform } from 'react-native';
 import { useGlobalStyles } from '../styles/globalStyles';
 import { useTheme as themeContext } from '../context/ThemeContext';
-import { useTheme, Divider, Switch, List } from 'react-native-paper';
+import { useTheme, Divider, Switch, List, RadioButton } from 'react-native-paper';
 import { useState } from 'react';
 import { openLink } from '../navigation/ExternalNavigation';
 
@@ -10,15 +10,48 @@ export default function SettingsScreen() {
     const theme = useTheme();
     const { toggleTheme } = themeContext();
 
+    const availableStartScreens = ['Home', 'Discover'];
     const [profileSettings, setProfileSettings] = useState({
         notifications: {
-            doNotDisturb: false,
+            push: true,
+            email: true,
         },
+        startScreen: 'Home'
     });
 
     return (
         <ScrollView contentContainerStyle={styles.scrollView} style={{backgroundColor: theme.colors.background}}>
-            <View style={styles.container}>
+            <View style={[styles.container, localStyles.mainView]}>
+                {/* General */}
+                <List.Section title='General' style={{...styles.listSection}} titleStyle={{...styles.listTitle, color: theme.colors.outline, ...styles.contentPaddingLeft}}>
+                    <List.Accordion
+                        title="Start screen"
+                        description={profileSettings.startScreen}
+                        titleStyle={{ ...styles.listTitle }}
+                        left={props => <List.Icon {...props} color={theme.colors.primary} style={styles.contentPaddingLeft} icon={profileSettings.startScreen === 'Home' ? 'home' : 'map-search'} />}
+                    >
+                        <RadioButton.Group
+                            onValueChange={value => setProfileSettings({ ...profileSettings, startScreen: value })}
+                            value={profileSettings.startScreen}
+                        >
+                            {(availableStartScreens).map((item, index) => (
+                                <RadioButton.Item
+                                    key={index}
+                                    label={item}
+                                    labelStyle={styles.listTitle}
+                                    value={item}
+                                    color={theme.colors.primary}
+                                    style={styles.contentPaddingLeft}
+                                />
+                            ))}
+
+                        </RadioButton.Group>
+                    </List.Accordion>
+
+                    <Divider style={styles.divider} />
+                </List.Section>
+
+                {/* Appearance */}
                 <List.Section title='Appearance' style={{...styles.listSection}} titleStyle={{...styles.listTitle, color: theme.colors.outline, ...styles.contentPaddingLeft}}>
                     <List.Item
                         title="Dark mode"
@@ -28,23 +61,32 @@ export default function SettingsScreen() {
                         right={(props) => <Switch {...props} value={theme.dark} onValueChange={toggleTheme} color={theme.colors.primary} style={styles.contentPaddingRight}/>}
                         subtitleStyle={{...styles.listDescription, color: theme.colors.outline }}
                     />
+                    <Divider style={styles.divider} />
                 </List.Section>
 
-                <Divider style={styles.divider} />
-
+                {/* Notifications */}
                 <List.Section title='Notifications' style={{...styles.listSection}} titleStyle={{...styles.listTitle, color: theme.colors.outline, ...styles.contentPaddingLeft}}>
                     <List.Item
-                        title="Do not disturb"
-                        description={profileSettings.notifications.doNotDisturb === false ? `Off`: `On`}
+                        title="Push"
+                        description={profileSettings.notifications.push === false ? `Off`: `On`}
                         titleStyle={{ ...styles.listTitle }}
-                        left={props => <List.Icon {...props} icon={profileSettings.notifications.doNotDisturb === false ? 'bell' : 'bell-off'} color={theme.colors.primary} style={styles.contentPaddingLeft} />}
-                        right={(props) => <Switch {...props} value={profileSettings.notifications.doNotDisturb} onValueChange={() => setProfileSettings({ ...profileSettings, notifications: { doNotDisturb: !profileSettings.notifications.doNotDisturb } })} color={theme.colors.primary} style={styles.contentPaddingRight}/>}
+                        left={props => <List.Icon {...props} icon={profileSettings.notifications.push === false ? 'bell-off' : 'bell'} color={theme.colors.primary} style={styles.contentPaddingLeft} />}
+                        right={(props) => <Switch {...props} value={profileSettings.notifications.push} onValueChange={() => setProfileSettings({ ...profileSettings, notifications: { ...profileSettings.notifications, push: !profileSettings.notifications.push } })} color={theme.colors.primary} style={styles.contentPaddingRight}/>}
                         subtitleStyle={{...styles.listDescription, color: theme.colors.outline }}
                     />
+
+                    <List.Item
+                        title="Email"
+                        description={profileSettings.notifications.email === false ? `Off`: `On`}
+                        titleStyle={{ ...styles.listTitle }}
+                        left={props => <List.Icon {...props} icon={profileSettings.notifications.email === false ? 'email-off' : 'email'} color={theme.colors.primary} style={styles.contentPaddingLeft} />}
+                        right={(props) => <Switch {...props} value={profileSettings.notifications.email} onValueChange={() => setProfileSettings({ ...profileSettings, notifications: { ...profileSettings.notifications, email: !profileSettings.notifications.email } })} color={theme.colors.primary} style={styles.contentPaddingRight}/>}
+                        subtitleStyle={{...styles.listDescription, color: theme.colors.outline }}
+                    />
+                    <Divider style={styles.divider} />
                 </List.Section>
 
-                <Divider style={styles.divider} />
-
+                {/* More */}
                 <List.Section title='More' style={{...styles.listSection}} titleStyle={{...styles.listTitle, color: theme.colors.outline, ...styles.contentPaddingLeft}}>
                     <List.Item
                         title="Rate the app"
@@ -66,3 +108,9 @@ export default function SettingsScreen() {
         </ScrollView>
     );
 }
+
+const localStyles = {
+    mainView: {
+        justifyContent: 'flex-start',
+    },
+};
