af63ec42ca8bbd53a8a1214bf0e0d0a7bf9d0f49
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-28 20:39:51 +0200

    Refactor TabNavigator to conditionally render screens based on user session

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -8,14 +8,17 @@ import { HomeStack } from './stacks/HomeStack';
 import { DiscoverStack } from './stacks/DiscoverStack';
 import { settingsConstants } from '../constants/storageConstants';
 import { loadData } from '../utils/asyncStorage';
+import { useAuth } from '../context/AuthContext';
+import { WelcomeStack } from './stacks/WelcomeStack';
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
+  const { session } = useAuth();
   const { theme } = useTheme();
   const styles = useGlobalStyles();
 
-  
+  // Load the startScreen from asyncStorage
   const [startScreen, setStartScreen] = useState(null);
 
   useEffect(() => {
@@ -26,11 +29,10 @@ export const TabNavigator = () => {
     fetchSettings();
   }, []);
 
-  // Load the tab navigator only when the startScreen is loaded from asyncStorage
+  // Load the tab navigator only when the neme of the startScreen is loaded from asyncStorage
   if (!startScreen) {
     return null;
   }
-
 
   return (
     <Tab.Navigator
@@ -41,42 +43,55 @@ export const TabNavigator = () => {
         tabBarActiveTintColor: theme.colors.primary,
       }}
     >
+      { session ? ( 
+        <>
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStack}
+            options={{
+              tabBarLabel: 'Home',
+              headerShown: false,
+              headerTitleAlign: 'left',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="home-variant-outline" size={size} color={color} />
+              ),
+            }}
+          /> 
+          <Tab.Screen
+            name="Discover"
+            component={DiscoverStack}
+            options={{
+              tabBarLabel: 'Discover',
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="map-search" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen 
+            name="Settings" 
+            component={SettingsScreen} 
+            options={{
+              headerStyle: styles.header,
+              headerTitleStyle: styles.headerTitle,
+              tabBarLabel: 'Settings',
+              headerTitleAlign: 'left',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="cog-outline" size={size} color={color} />
+              ),
+            }}
+          />
+        </>
+      ) : 
       <Tab.Screen
-        name="HomeStack"
-        component={HomeStack}
-        options={{
-          tabBarLabel: 'Home',
+        name="WelcomeStack"
+        component={WelcomeStack}
+        options={{ 
+          tabBarButton: () => null, // Hide the button
           headerShown: false,
-          headerTitleAlign: 'left',
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="home-variant-outline" size={size} color={color} />
-          ),
-        }}
-      /> 
-      <Tab.Screen
-        name="Discover"
-        component={DiscoverStack}
-        options={{
-          tabBarLabel: 'Discover',
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="map-search" size={size} color={color} />
-          ),
-        }}
+        }} 
       />
-      <Tab.Screen 
-        name="Settings" 
-        component={SettingsScreen} 
-        options={{
-          headerStyle: styles.header,
-          headerTitleStyle: styles.headerTitle,
-          tabBarLabel: 'Settings',
-          headerTitleAlign: 'left',
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="cog-outline" size={size} color={color} />
-          ),
-        }}
-      />
+    }
     </Tab.Navigator>
   );
 };
