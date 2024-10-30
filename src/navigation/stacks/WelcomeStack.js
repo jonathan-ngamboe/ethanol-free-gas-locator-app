5382fe1f4695403315a1f4e4833f059028e2117a
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-30 12:27:43 +0200

    Refactor WelcomeStack to use AuthScreen instead of AuthStack

--- src/navigation/stacks/WelcomeStack.js
+++ src/navigation/stacks/WelcomeStack.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useTheme } from '../../context/ThemeContext';
 import { useGlobalStyles } from '../../styles/globalStyles';
-import { AuthStack } from './AuthStack';
+import AuthScreen from '../../screens/AuthScreen';
 import WelcomeScreen from '../../screens/WelcomeScreen';
+import { View, StyleSheet } from 'react-native';
+import { Text, Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
 export const WelcomeStack = () => {
   const { theme } = useTheme();
   const styles = useGlobalStyles();
+  const navigation = useNavigation();
 
   return (
     <Stack.Navigator
@@ -19,20 +23,46 @@ export const WelcomeStack = () => {
         headerTintColor: theme.colors.primary,
       }}
     >
-      <Stack.Screen 
+      <Stack.Screen
         name="Welcome"
         component={WelcomeScreen}
         options={{
-            headerShown: false,
+          headerTitle: '',
+          headerRight: () => (
+            <View style={localStyles.headerRightContainer}>
+              <Text style={[localStyles.headerTitle, { color: theme.colors.outline }]}>
+                Already have an account?
+              </Text>
+              <Button
+                mode="text"
+                icon="login"
+                onPress={() => navigation.navigate('AuthScreen', { screen: 'Sign in' })}
+              >
+                Sign in
+              </Button>
+            </View>
+          ),
         }}
       />
-      <Stack.Screen 
-        name="AuthStack"
-        component={AuthStack}
+      <Stack.Screen
+        name="AuthScreen"
+        component={AuthScreen}
         options={{
-          headerShown: false,
+          headerTitle: '',
+          headerBackTitleVisible: false,
         }}
       />
     </Stack.Navigator>
   );
 };
+
+const localStyles = StyleSheet.create({
+  headerRightContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 16,
+  },
+  headerTitle: {
+    marginRight: 8,
+  },
+});
