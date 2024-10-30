be1e1b9ce1ba908f54c61866bb43acfaeb2b703f
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-10-31 01:00:49 +0200

    Add userService.js to fetch and format user data

---
+++ src/services/userService.js
@@ -0,0 +1,28 @@
+import { supabase } from "../../lib/supabase";
+
+
+export const getUser = async (showSnackbar) => {
+    try {
+        const { data, error } = await supabase.auth.getUser();
+        
+        if (error) {
+            showSnackbar('Error fetching user data', error);
+            return { error, data: null };
+        }
+
+        const randomNum = Math.floor(Math.random() * 1000);
+        const formattedUser = {
+            firstName: data.user?.user_metadata?.first_name,
+            lastName: data.user?.user_metadata?.last_name,
+            email: data.user?.email,
+            isPro: data.user?.user_metadata?.is_pro || false,
+            avatar: data.user?.user_metadata?.avatar_url || 'https://avatar.iran.liara.run/public',
+            bannerImage: data.user?.user_metadata?.banner_image_url || `https://picsum.photos/seed/${randomNum}/500/200`,
+        };
+
+        return { data: { user: formattedUser }, error: null };
+    } catch (error) {
+        showSnackbar('An unexpected error occurred while fetching user data', error);
+        return { error, data: null };
+    }
+};
