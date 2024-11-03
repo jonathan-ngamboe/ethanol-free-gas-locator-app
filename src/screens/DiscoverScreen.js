4978b2cf31cf877ade45e05c7d27017868c56f01
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-11-03 02:31:23 +0200

    Refactor DiscoverScreen to handle user location search, apply filters and sort

--- src/screens/DiscoverScreen.js
+++ src/screens/DiscoverScreen.js
@@ -10,18 +10,15 @@ import StationList from "../components/StationList";
 import Filters from "../components/Filters";
 import Animated, { FadeIn, FadeOut, runOnJS } from 'react-native-reanimated';
 import React, { useState, useRef, useCallback, useEffect } from "react";
-import { getAllStations, getNearbyStations } from "../services/stationService";
+import { getNearbyStations } from "../services/stationService";
 import * as Location from 'expo-location';
 import { useSnackbar } from '../context/SnackbarContext';
-
-
+import { initialFilters, initialViewMode, initialSortBy } from "../constants/filtersConstants";
 
 export default function DiscoverScreen({navigation}) {
     const route = useRoute();
     const params = route.params;
     const theme = useTheme();
-    const [viewMode, setViewMode] = useState('detailed');
-    const [filters, setFilters] = useState({});
     const { showSnackbar } = useSnackbar();
     // State to handle animations
     const [shouldRenderCarousel, setShouldRenderCarousel] = useState(true);
@@ -32,7 +29,8 @@ export default function DiscoverScreen({navigation}) {
     const [stations, setStations] = useState([]);
     const [loading, setLoading] = useState(false);
     // Location
-    const [userLocation, setLocation] = useState(null);
+    const [userLocation, setUserLocation] = useState(null);
+    const [searchLocation, setSearchLocation] = useState(''); // Location from the search bar (street, city, state, postal code)
     // Search bar handling
     const searchBarRef = useRef(null);  
     const {openKeyboard = false} = params || {};
@@ -40,6 +38,13 @@ export default function DiscoverScreen({navigation}) {
     const timerRef = useRef(null);
     // Index of the selected station on the map
     const [selectedStationIndex, setSelectedStationIndex] = useState(null);
+    // Filters
+    const [filters, setFilters] = useState(initialFilters);
+    // View mode
+    const [viewMode, setViewMode] = useState(initialViewMode);
+    // Sort by
+    const [sortBy, setSortBy] = useState(initialSortBy);
+    
 
     // Filter icon
     const filterIcon = (
@@ -53,19 +58,54 @@ export default function DiscoverScreen({navigation}) {
 
     ///// Callbacks /////
 
-    // Fetch stations
-    const getStations = useCallback((longitude, latitude) => {
+    // Fetch userLocation
+    const getLocation = useCallback(async () => {
         setLoading(true);
-        getNearbyStations(longitude, latitude)
-            .then(stations => {
-                setStations(stations);
-                setLoading(false);
-            })
-            .catch(error => {
-                showSnackbar('Error fetching stations:', error);
-                console.error('Error fetching stations:', error);
+        try {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                showSnackbar('Permission to access location was denied. Please enable location services to view nearby stations.');
+                return;
+            }
+            
+            const userLocation = await Location.getCurrentPositionAsync({});
+            setUserLocation(userLocation.coords);
+            
+            setSearchLocation('');
+
+            performSearch({ 
+                longitude: userLocation.coords.longitude, 
+                latitude: userLocation.coords.latitude, 
+                filters 
             });
-    }, [userLocation]);
+
+            handleSort(sortBy);
+        } catch (error) {
+            showSnackbar('Error fetching location:', error);
+            console.error('Error fetching location:', error);
+        } finally {
+            setLoading(false);
+        }
+    }, [showSnackbar, searchLocation, filters, performSearch]);
+
+    // Fetch stations
+    const performSearch = useCallback(async (searchParams) => {
+        const { location, longitude, latitude, filters } = searchParams;
+        setLoading(true);
+        try {
+            const stations = await getNearbyStations(
+                location, 
+                longitude, 
+                latitude, 
+                filters
+            );
+            setStations(stations);
+        } catch (error) {
+            showSnackbar('Error fetching stations:', error);
+        } finally {
+            setLoading(false);
+        }
+    }, [showSnackbar]);
 
     // Modal control functions
     const minimizeModal = useCallback(() => {
@@ -142,19 +182,58 @@ export default function DiscoverScreen({navigation}) {
         }
     }, [isFiltersVisible, viewMode, openModal]);
 
+    const handleSort = useCallback((newSortBy) => {
+        setSortBy(newSortBy);
+        setStations(prevStations => {
+            const sortedStations = [...prevStations];
+            if (newSortBy === 'nearest') {
+                sortedStations.sort((a, b) => a.distance - b.distance);
+            } else if (newSortBy === 'name') {
+                sortedStations.sort((a, b) => a.station_name.localeCompare(b.station_name));
+            }
+            return sortedStations;
+        });
+    }, []);
+
     const renderFilters = useCallback(() => (
         <Filters 
-            closeFilters={closeFilters} 
-            viewMode={viewMode} 
+            sortBy={sortBy}
+            setSortBy={handleSort}
+            viewMode={viewMode}
             setViewMode={handleViewModeChange} 
-            handleFilters={handleApplyFilters}
+            onApply={handleApplyFilters}
+            filters={filters}
+            setFilters={setFilters}
         />
     ), [closeFilters, viewMode, handleViewModeChange]);
 
-    handleApplyFilters = useCallback((filters) => {
-        setFilters(filters);
-        console.log('Filters applied:', filters);
-    }, [filters]);
+    const handleApplyFilters = useCallback((newFilters) => {
+        setFilters(prevFilters => {
+            const updatedFilters = { ...prevFilters, ...newFilters };
+            
+            // Perform a new search with the updated filters
+            if (searchLocation.trim()) {
+                performSearch({ location: searchLocation.trim(), filters: updatedFilters });
+            } else if (userLocation) {
+                performSearch({ 
+                    longitude: userLocation.longitude, 
+                    latitude: userLocation.latitude, 
+                    filters: updatedFilters 
+                });
+            }
+            
+            return updatedFilters;
+        });
+    }, [searchLocation, userLocation, performSearch]);
+
+    const handleSearch = useCallback(() => {
+        if (!searchLocation.trim()) return;
+        
+        performSearch({ 
+            location: searchLocation.trim(), 
+            filters 
+        });
+    }, [searchLocation, filters, performSearch]);
 
     // Focus on search bar when the screen is focused and openKeyboard is true
     useFocusEffect(
@@ -199,13 +278,6 @@ export default function DiscoverScreen({navigation}) {
 
     ///// useEffects /////
 
-    // Fetch stations when userLocation changes
-    useEffect(() => {
-        if (userLocation) {
-            getStations(userLocation?.longitude, userLocation?.latitude);
-        }
-    }, [userLocation]);
-
     // Cleanup function for timers
     useEffect(() => {
         return () => {
@@ -217,33 +289,10 @@ export default function DiscoverScreen({navigation}) {
 
     // Get user's userLocation when the component mounts
     useEffect(() => {
-        function getLocation() {
-            setLoading(true);
-            Location.requestForegroundPermissionsAsync()
-                .then(response => {
-                    if (response.status !== 'granted') {
-                        showSnackbar('Permission to access userLocation was denied. Please enable userLocation services to view nearby stations.');
-                        return;
-                    }
-                    return Location.getCurrentPositionAsync({});
-                })
-                .then(userLocation => {
-                    if (userLocation) {
-                        setLocation(userLocation.coords);
-                    }
-                })
-                .catch(error => {
-                    showSnackbar('Error fetching userLocation:', error);
-                })
-                .finally(() => {
-                    setLoading(false);
-                });
-        }
-        
         getLocation();
     }, []);
-    
 
+    
     return (
         <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{flex: 1}}>
             <BottomSheetModalProvider>
@@ -261,9 +310,18 @@ export default function DiscoverScreen({navigation}) {
                         <SearchBar
                             barRef={searchBarRef}
                             leftIcon='map-marker'
-                            onLeftIconPress={() => console.log('Map icon pressed')}
+                            onLeftIconPress={() => getLocation()}
                             rightComponent={filterIcon}
-                            isLoading={loading}  
+                            isLoading={loading}
+                            placeholder={userLocation && !searchLocation ? 'Current location' : 'Search by location'}
+                            onSubmit={() => handleSearch()}
+                            searchQuery={searchLocation}
+                            setSearchQuery={
+                                (query) => {setSearchLocation(query)
+                                if (query.trim() === '') {
+                                    setUserLocation(null); // Clear userLocation when search bar is empty. If the user needs to search again with the current userLocation, they can click the map left icon
+                                }
+                            }}
                         />
                     </View>
 
