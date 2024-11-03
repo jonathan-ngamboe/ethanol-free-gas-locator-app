c6cd92b58f1acbdc16ccf6b0940f82814313a6a6
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-11-03 02:18:25 +0200

    Add filtersConstants.js with initial filters, view mode, and sort by options

---
+++ src/constants/filtersConstants.js
@@ -0,0 +1,15 @@
+// Initial filters
+export const initialFilters = {
+    status: [],
+    cards_accepted: [],
+    access: '',
+    e85_other_ethanol_blends: [],
+    e85_blender_pump: false,
+    radius: 50,
+};
+
+// Initial view mode
+export const initialViewMode = 'detailed';
+
+// Initial sort by
+export const initialSortBy = 'nearest';
