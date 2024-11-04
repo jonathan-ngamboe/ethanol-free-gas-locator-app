979c1e26ebb797828677742aa9f8616a1586a323
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2024-11-05 01:30:43 +0200

    Remove console logs

--- src/navigation/ExternalNavigation.js
+++ src/navigation/ExternalNavigation.js
@@ -2,8 +2,6 @@ import { Alert, Linking, Platform, Share } from 'react-native';
 import * as Clipboard from 'expo-clipboard';
 
 export async function openMap({ destinationName, destinationLat, destinationLon }) {
-    console.log('destinationName', destinationName)
-    console.log('destinationLat', destinationLat)
     const scheme = Platform.select({ ios: 'maps://0,0?q=', android: 'geo:0,0?q=' });
     const latLng = `${destinationLat},${destinationLon}`;
     // Remove any # symbols from the destination name to avoid any issues with the Maps URL
